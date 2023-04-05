1b39e834d1f60529e63a29f0cbe3472fd9378a70
MaRii0NiiSTaL <mnistal22@izarraitz.eus>
2023-04-05 12:07:39 +0200

    mario aldaketa 12:07

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ namespace ERRONKA7
             loginForm = new login();
             Application.Run(loginForm);
 
+            Marionetik aldaketa
+
 
 
             Unaik,... aldaketa hau sartu du
